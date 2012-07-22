2d4b20dc3aaf127d042b1e593c8266f94e9c1475
Igor Petruk <igor.petrouk@gmail.com>
2012-07-22 20:36:19 +0300

    Setting pending articles visible on small feed

--- src/main/java/org/guiceae/main/web/MainController.java
+++ src/main/java/org/guiceae/main/web/MainController.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.sun.jersey.api.view.Viewable;
 import org.guiceae.main.model.Article;
 import org.guiceae.main.repositories.ArticleRepository;
+import org.guiceae.util.UserPrincipalHolder;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,11 +17,15 @@ public class MainController {
     @Inject
     ArticleRepository articleRepository;
 
+    @Inject
+    UserPrincipalHolder userPrincipalHolder;
+
     @GET
     public Viewable getMain() {
         Map<String, List<? extends Article>> it = new HashMap<String, List<? extends Article>>();
-        it.put("newsFeed", articleRepository.getFeed("news", true, 0));
-        it.put("charityFeed", articleRepository.getFeed("charity", true, 0));
+        boolean onlyPublished = !userPrincipalHolder.get().contains("cm");
+        it.put("newsFeed", articleRepository.getFeed("news", onlyPublished, 0));
+        it.put("charityFeed", articleRepository.getFeed("charity", onlyPublished, 0));
         return new Viewable("/main.jsp", it);
     }
 }
